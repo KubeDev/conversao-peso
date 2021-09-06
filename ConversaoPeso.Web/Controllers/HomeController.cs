2e2a09b0695d0776f89f8b1df16c9e665a5f8f34
Fabricio Veronez <fabricioveronez@gmail.com>
2021-09-06 12:59:23 -0300

    Commit Inicial

---
+++ ConversaoPeso.Web/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ConversaoPeso.Web.Models;
+
+namespace ConversaoPeso.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Index(string selectTemp, decimal valorRef)
+        {
+            if (selectTemp.Equals("1"))
+            {
+                ViewData["unidade"] = "quilo";
+                ViewData["valor"] = valorRef / 1000;
+            }
+            else
+            {
+                ViewData["unidade"] = "grama";
+                ViewData["valor"] = valorRef * 1000;
+            }
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# conversao-peso
